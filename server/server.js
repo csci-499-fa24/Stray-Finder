6a2cec6be4f172fca0914e1786b09024b700a8b7
Alejandro Botero <102490384+alejobotero@users.noreply.github.com>
2024-10-01 16:00:36 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const port = process.env.PORT || 8080
 // Load DB then start server
 const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URL)
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server started on port ${port}`))
     } catch (error) {
         console.log(error)
