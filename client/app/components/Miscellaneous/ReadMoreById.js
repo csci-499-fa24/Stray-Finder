cbd911bc4e6681fc3dcbdbdaf4a538fe5bb81af0
ahuang7101 <33328014+ahuang7101@users.noreply.github.com>
2024-10-10 09:59:34 -0400

    added ReadMoreById component and page for it

--- client/app/components/Miscellaneous/ReadMoreById.js
+++ client/app/components/Miscellaneous/ReadMoreById.js
@@ -4,17 +4,17 @@ import { GoogleMap, Marker, LoadScriptNext } from '@react-google-maps/api';
 import { useEffect, useState } from 'react';
 
 const ReadMoreById = ({ id }) => {
-    const [stray, setStray] = useState([]); // State for storing stray data
+    const [animals, setAnimals] = useState([]); // State for storing animals data
     const [loading, setLoading] = useState(true); // State for loading 
     const [mapCenter, setMapCenter] = useState({ lat: 51.505, lng: -0.09 }); // Default coordinates for the map
 
     useEffect(() => {
         const fetchStrayData = async () =>{
             try{
-                const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/spottedstray/${id}`);
+                const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/animal/${id}`);
                 const data = await response.json();
 
-                setStray(data); // stores data in the state
+                setAnimals(data); // stores data in the state
                 // Set map center based on coordinates from JSON
                 if (data.animal && data.animal.coordinates) {
                     setMapCenter({
@@ -23,7 +23,7 @@ const ReadMoreById = ({ id }) => {
                     });
                 }
             } catch(error) {
-                console.error('Error fetching stray data: ', error);
+                console.error('Error fetching animals data: ', error);
             } finally {
                 setLoading(false); // stop loading when the fetching of data is complete
             }
@@ -41,29 +41,29 @@ const ReadMoreById = ({ id }) => {
     return (
         <div className="col">
             <div className="card m-3 p-0">
-                <h1 className="card-title">{stray.animal.name}</h1>
+                <h1 className="card-title">{animals.animal.name}</h1>
                 <div className="mx-3 border p-2" >
-                    <img src={stray.animal.imageUrl} className="card-img-top" alt={stray.animal.name} />
+                    <img src={animals.animal.imageUrl} className="card-img-top" alt={animals.animal.name} />
                 </div>
                 <div className="card-body">
-                    <p className="card-text">{stray.animal.description}</p>
+                    <p className="card-text">{animals.animal.description}</p>
                 </div>
                 <ul className="list-group list-group-flush">
-                    <li className="list-group-item">Species: {stray.animal.species}</li>
-                    <li className="list-group-item">Breed: {stray.animal.breed}</li>
-                    <li className="list-group-item">Gender: {stray.animal.gender}</li>
-                    <li className="list-group-item">State: {stray.animal.state}</li>
-                    <li className="list-group-item">Date Reported: {new Date(stray.animal.dateReported).toLocaleDateString('en-US', {
+                    <li className="list-group-item">Species: {animals.animal.species}</li>
+                    <li className="list-group-item">Breed: {animals.animal.breed}</li>
+                    <li className="list-group-item">Gender: {animals.animal.gender}</li>
+                    <li className="list-group-item">State: {animals.animal.state}</li>
+                    <li className="list-group-item">Date Reported: {new Date(animals.animal.dateReported).toLocaleDateString('en-US', {
                         year: 'numeric',
                         month: 'long',
                         day: 'numeric'
-                    })}, {new Date(stray.animal.dateReported).toLocaleTimeString('en-US', {
+                    })}, {new Date(animals.animal.dateReported).toLocaleTimeString('en-US', {
                         hour: 'numeric',
                         minute: 'numeric'
                     })}</li>
                     <li className="list-group-item">Coordinates:<br/>
-                    Latitude: {stray.animal.coordinates.coordinates[1]}<br/>
-                    Longitude: {stray.animal.coordinates.coordinates[0]}</li>
+                    Latitude: {animals.animal.coordinates.coordinates[1]}<br/>
+                    Longitude: {animals.animal.coordinates.coordinates[0]}</li>
 
                 </ul>
                 <div className = "mx-3 mt-2">
@@ -77,10 +77,10 @@ const ReadMoreById = ({ id }) => {
                             center={mapCenter}
                             zoom={17}
                         >
-                            {/* Marker based on stray's coordinates */}
+                            {/* Marker based on animals's coordinates */}
                             <Marker position={mapCenter} 
                             // icon={{
-                            //     url: `${stray.animal.image}`, // URL to your custom marker image
+                            //     url: `${animals.animal.image}`, // URL to your custom marker image
                             //     scaledSize: new window.google.maps.Size(40, 40) // Custom size (optional)
                             // }}
                             />
