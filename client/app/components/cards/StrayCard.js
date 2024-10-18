27e7e287039612ceffcbc60f0687e267a4a9f47b
EbubeDivinee <oluebubeonwuamaegbu@gmail.com>
2024-10-17 21:13:59 -0400

    Consistent image sizing

--- client/app/components/cards/StrayCard.js
+++ client/app/components/cards/StrayCard.js
@@ -1,9 +1,12 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 const StrayCard = ({ id, name, image, species, breed, gender, state, description }) => (
     <div className="col">
         <div className="card m-3 p-0">
-            <img src={image} className="card-img-top" alt={name} />
+            {/* Container for the image to maintain square aspect ratio */}
+            <div className="img-container">
+                <img src={image} className="card-img-top" alt={name} />
+            </div>
             <div className="card-body">
                 <h5 className="card-title">{name}</h5>
                 <p className="card-text">{description}</p>
@@ -20,6 +23,6 @@ const StrayCard = ({ id, name, image, species, breed, gender, state, description
             </div>
         </div>
     </div>
-)
+);
 
-export default StrayCard
+export default StrayCard;
