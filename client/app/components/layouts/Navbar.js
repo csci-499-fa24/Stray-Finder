f9c1f6d9225953dc56e379ebb3d50ca844608803
Rod <strayfinder24@gmail.com>
2024-10-22 15:10:34 -0400

    design changes

--- client/app/components/layouts/Navbar.js
+++ client/app/components/layouts/Navbar.js
@@ -1,10 +1,8 @@
 'use client'
+
 import Link from 'next/link'
-import useAuth from '@/app/hooks/useAuth'
 
 const Navbar = () => {
-    const { isAuthenticated, user } = useAuth()
-
     return (
         <nav className="navbar navbar-expand-lg justify-content-center custom-navbar">
             <div className="container-fluid d-flex">
@@ -19,50 +17,39 @@ const Navbar = () => {
                 >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-
+                
                 {/* Navbar Brand */}
-                <Link className="navbar-brand" href="/">
+                <Link className="navbar-brand highlight-brand stray-finder-logo" href="/">
                     Stray Finder
                 </Link>
-
+                
                 <div
                     className="collapse navbar-collapse"
                     id="navbarSupportedContent"
                 >
                     <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                        
                         {/* Home Page Link */}
                         <li className="nav-item">
-                            <Link
-                                className="nav-link active"
-                                aria-current="page"
-                                href="/"
-                            >
+                            <Link className="nav-link active" aria-current="page" href="/">
                                 Home
                             </Link>
                         </li>
-
+                        
                         {/* About Page Link */}
                         <li className="nav-item">
-                            <Link
-                                className="nav-link active"
-                                aria-current="page"
-                                href="/about"
-                            >
+                            <Link className="nav-link active" aria-current="page" href="/about">
                                 About
                             </Link>
                         </li>
-
+                        
                         {/* Contact Page Link */}
                         <li className="nav-item">
-                            <Link
-                                className="nav-link active"
-                                aria-current="page"
-                                href="/contact"
-                            >
+                            <Link className="nav-link active" aria-current="page" href="/contact">
                                 Contact
                             </Link>
                         </li>
-
+                        
                         {/* Dropdown Menu */}
                         <li className="nav-item dropdown">
                             <Link
@@ -76,46 +63,48 @@ const Navbar = () => {
                             </Link>
                             <ul className="dropdown-menu">
                                 <li>
-                                    <Link
-                                        className="dropdown-item"
-                                        href="/report-lost"
-                                    >
+                                    <Link className="dropdown-item" href="/report-lost">
                                         Report Lost
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link
-                                        className="dropdown-item"
-                                        href="/report-found"
-                                    >
+                                    <Link className="dropdown-item" href="/report-found">
                                         Report Found
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link
-                                        className="dropdown-item"
-                                        href="/report-stray"
-                                    >
+                                    <Link className="dropdown-item" href="/report-stray">
                                         Report Stray
                                     </Link>
                                 </li>
                             </ul>
                         </li>
                     </ul>
+                    
+                    {/* Search Form */}
+                    <form className="d-flex" role="search">
+                        <input
+                            className="form-control me-2"
+                            type="search"
+                            placeholder="e.g. name, breed"
+                            aria-label="Search"
+                        />
+                        <button
+                            className="btn btn-outline-success search-button"
+                            type="submit"
+                        >
+                            Search
+                        </button>
+                    </form>
                 </div>
 
-                {/* Conditional Login/Welcome User */}
+                {/* Login Link */}
                 <div className="p-3 ml-auto">
-                    {isAuthenticated && user ? (
-                        <h6>{`Welcome, ${user.username}`}</h6>
-                    ) : (
-                        <Link href="/login">Login</Link>
-                    )}
-                    {/* Profile icon coming some time later */}
+                    <Link href="/login">Login</Link>
                 </div>
             </div>
         </nav>
     )
 }
 
-export default Navbar
+export default Navbar;
