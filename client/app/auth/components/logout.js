05b889fd07ab16943c63a4bf25fdcf0ba16eadb5
Raed Abuzaid <abuzaidraed13@gmail.com>
2024-10-29 13:11:35 -0400

    Update logout.js
    Delete cookie by setting expiration to 1970. Seems unusual but apparently this is industry standard

--- client/app/auth/components/logout.js
+++ client/app/auth/components/logout.js
@@ -1,22 +1,15 @@
 'use client';
-import { useRouter } from 'next/navigation'; // Use the App Router's `useRouter`
+import { useRouter } from 'next/navigation';
 
 const LogoutButton = () => {
     const router = useRouter();
 
-    const handleLogout = async () => {
+    const handleLogout = () => {
         try {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/auth/logout`, {
-                method: 'POST',
-                credentials: 'include', // Include cookies in the request
-            });
+            // Clear the token cookie by setting an expired date
+            document.cookie = 'token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;';
+            console.log('Token cookie removed');
 
-            if (!response.ok) {
-                throw new Error('Failed to logout');
-            }
-
-            const result = await response.json();
-            console.log(result.message);
             // Redirect to the login page or homepage after logging out
             router.push('/auth'); // Adjust as needed
         } catch (error) {
