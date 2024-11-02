bfe504bad2fb0f54c4875575565e261cccf80654
Alejandro Botero <102490384+alejobotero@users.noreply.github.com>
2024-11-02 16:28:15 -0400

    Update firebase.js
    security

--- client/app/notifications/firebase.js
+++ client/app/notifications/firebase.js
@@ -2,32 +2,29 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging, getToken } from "firebase/messaging";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDAAMNLBHKw0ar2RR4iidrWI5sF9lvk52M",
-  authDomain: "strayfinder-a092f.firebaseapp.com",
-  projectId: "strayfinder-a092f",
-  storageBucket: "strayfinder-a092f.firebasestorage.app",
-  messagingSenderId: "293970270878",
-  appId: "1:293970270878:web:03152a189e81d938dbf98f",
-  measurementId: "G-JDBC1XTL7G"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 
-
-export const generateToken = async() => {
+export const generateToken = async () => {
     const permission = await Notification.requestPermission();
     console.log(permission);
-    if(permission == "granted"){
+    if (permission === "granted") {
         const token = await getToken(messaging, {
-            vapidKey: "BHvY2x7sMjrhzINeqjr_DH9gJpBLhxGS_NN2fj92BGgBeUxCGc3IDPS2YFmTnpXEV6EqNTT1m49lBePk5baNneI",
-    });
-    console.log(token)
+            vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY
+        });
+        console.log(token);
     }
 };
 
-export { messaging }
+export { messaging };
