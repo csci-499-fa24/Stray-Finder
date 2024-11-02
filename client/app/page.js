208606eb04fb8a9a25c5d059adba903cfd4fc212
Alejandro Botero <102490384+alejobotero@users.noreply.github.com>
2024-11-02 19:23:31 -0400

    fixed DOM bug

--- client/app/page.js
+++ client/app/page.js
@@ -7,12 +7,10 @@ import SpottedStrayCard from './components/cards/SpottedStrayCard'
 import FeaturedStrays from './components/cards/FeaturedAnimals'
 import Footer from './components/layouts/Footer'
 import Slideshow from './components/layouts/Slideshow/Slideshow'
-import toast, { Toaster } from 'react-hot-toast'
 
 export default function Home() {
     return (
         <div>
-            <Toaster position='top-right' />
             <Navbar />
             {/*<Breadcrumb />*/}
             <Slideshow />
