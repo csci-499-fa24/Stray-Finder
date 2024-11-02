f068f8e4ec61aef6a707d21f9002fca607eddc9e
Raed <abuzaidraed13@gmail.com>
2024-11-02 00:53:45 -0400

    Could sworn i removed the log already

--- server/cloudinary/config.js
+++ server/cloudinary/config.js
@@ -8,10 +8,4 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_URL.split(':')[2].split('@')[0],
 })
 
-console.log('Cloudinary Config:', {
-    cloud_name: process.env.CLOUDINARY_URL.split('@')[1],
-    api_key: process.env.CLOUDINARY_URL.split('://')[1].split(':')[0],
-    api_secret: process.env.CLOUDINARY_URL.split(':')[2].split('@')[0],
-})
-
 module.exports = cloudinary
