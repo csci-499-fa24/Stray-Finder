0129127addd700f3fa70495d631b51717b82f99b
nicholasm112 <nicholas.morgan12@myhunter.cuny.edu>
2024-11-10 16:54:11 -0500

    Added extra authentication check for profile page if user tries to force access using userid in address

--- client/app/profile/[id]/page.js
+++ client/app/profile/[id]/page.js
@@ -3,11 +3,19 @@
 import Navbar from "@/app/components/layouts/Navbar/Navbar";
 import UserProfile from "./components/UserProfile";
 import Footer from "@/app/components/layouts/Footer/Footer";
-import { useParams } from 'next/navigation'; // Import useParams for unwrapping params
+import { useParams, useRouter } from 'next/navigation';
+import useAuth from '@/app/hooks/useAuth';
 
 const ProfilePage = () => {
+    const { isAuthenticated } = useAuth();
+    const router = useRouter();
     const params = useParams();
     const id = params.id;
+
+    if (isAuthenticated === false) {
+        router.push('/auth');
+        return null;
+    }
 
     return (
         <div>
